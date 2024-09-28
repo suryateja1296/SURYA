064f47144029fea0c5a5764a478e2122a000f378
suryateja1296 <166375949+suryateja1296@users.noreply.github.com>
2024-09-28 14:33:29 +0530

    Add files via upload

---
+++ exp-1c/exp-1c/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Current Time: " + DateTime.Now.ToString("hh:mm:ss tt"));
+            System.Threading.Thread.Sleep(1000); // wait for 1 second
+        }
+    }
+}
