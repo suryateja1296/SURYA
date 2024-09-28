b412b80db302715257ec8143a3e80e17ddc6c68c
suryateja1296 <166375949+suryateja1296@users.noreply.github.com>
2024-09-28 14:42:48 +0530

    Add files via upload

---
+++ EXP-5/forms.cs/Form1.Designer.vb
@@ -0,0 +1,573 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnPer = New RoundButton()
+        btn8 = New RoundButton()
+        btn9 = New RoundButton()
+        btnMul = New RoundButton()
+        btn7 = New RoundButton()
+        btnD = New RoundButton()
+        btnAC = New RoundButton()
+        btnBack = New RoundButton()
+        btn6 = New RoundButton()
+        btnEqual = New RoundButton()
+        btn3 = New RoundButton()
+        btn2 = New RoundButton()
+        btn0 = New RoundButton()
+        btn00 = New RoundButton()
+        btn5 = New RoundButton()
+        btnAdd = New RoundButton()
+        btnDot = New RoundButton()
+        btnSub = New RoundButton()
+        btn1 = New RoundButton()
+        btn4 = New RoundButton()
+        btnLog = New RoundButton()
+        btnCos = New RoundButton()
+        btnTan = New RoundButton()
+        btnRad = New RoundButton()
+        btnSin = New RoundButton()
+        btnLn = New RoundButton()
+        btnInv = New RoundButton()
+        btnDeg = New RoundButton()
+        btnpi = New RoundButton()
+        btnsqrt = New RoundButton()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnPer
+        ' 
+        btnPer.BackColor = Color.Silver
+        btnPer.BackgroundImageLayout = ImageLayout.Stretch
+        btnPer.FlatStyle = FlatStyle.Popup
+        btnPer.Location = New Point(342, 194)
+        btnPer.Name = "btnPer"
+        btnPer.Size = New Size(44, 38)
+        btnPer.TabIndex = 36
+        btnPer.Text = "%"
+        btnPer.UseVisualStyleBackColor = False
+        ' 
+        ' btn8
+        ' 
+        btn8.BackColor = SystemColors.InactiveCaptionText
+        btn8.BackgroundImageLayout = ImageLayout.Stretch
+        btn8.FlatStyle = FlatStyle.Popup
+        btn8.ForeColor = SystemColors.ButtonHighlight
+        btn8.Location = New Point(342, 238)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(44, 38)
+        btn8.TabIndex = 37
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = False
+        ' 
+        ' btn9
+        ' 
+        btn9.BackColor = SystemColors.InactiveCaptionText
+        btn9.BackgroundImageLayout = ImageLayout.Stretch
+        btn9.FlatStyle = FlatStyle.Popup
+        btn9.ForeColor = SystemColors.ButtonHighlight
+        btn9.Location = New Point(388, 238)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(44, 38)
+        btn9.TabIndex = 38
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = False
+        ' 
+        ' btnMul
+        ' 
+        btnMul.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        btnMul.BackgroundImageLayout = ImageLayout.Stretch
+        btnMul.FlatStyle = FlatStyle.Popup
+        btnMul.Location = New Point(442, 238)
+        btnMul.Name = "btnMul"
+        btnMul.Size = New Size(44, 38)
+        btnMul.TabIndex = 39
+        btnMul.Text = "*"
+        btnMul.UseVisualStyleBackColor = False
+        ' 
+        ' btn7
+        ' 
+        btn7.BackColor = SystemColors.InactiveCaptionText
+        btn7.BackgroundImageLayout = ImageLayout.Stretch
+        btn7.FlatStyle = FlatStyle.Popup
+        btn7.ForeColor = SystemColors.ButtonHighlight
+        btn7.Location = New Point(292, 238)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(44, 38)
+        btn7.TabIndex = 40
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = False
+        ' 
+        ' btnD
+        ' 
+        btnD.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        btnD.BackgroundImageLayout = ImageLayout.Stretch
+        btnD.FlatStyle = FlatStyle.Popup
+        btnD.Location = New Point(442, 194)
+        btnD.Name = "btnD"
+        btnD.Size = New Size(44, 38)
+        btnD.TabIndex = 41
+        btnD.Text = "/"
+        btnD.UseVisualStyleBackColor = False
+        ' 
+        ' btnAC
+        ' 
+        btnAC.BackColor = Color.Silver
+        btnAC.BackgroundImageLayout = ImageLayout.Stretch
+        btnAC.FlatStyle = FlatStyle.Popup
+        btnAC.Location = New Point(292, 194)
+        btnAC.Name = "btnAC"
+        btnAC.Size = New Size(44, 38)
+        btnAC.TabIndex = 42
+        btnAC.Text = "AC"
+        btnAC.UseVisualStyleBackColor = False
+        ' 
+        ' btnBack
+        ' 
+        btnBack.BackColor = Color.Silver
+        btnBack.BackgroundImageLayout = ImageLayout.Stretch
+        btnBack.FlatStyle = FlatStyle.Popup
+        btnBack.Location = New Point(392, 194)
+        btnBack.Name = "btnBack"
+        btnBack.Size = New Size(44, 38)
+        btnBack.TabIndex = 43
+        btnBack.Text = "⌫"
+        btnBack.UseVisualStyleBackColor = False
+        ' 
+        ' btn6
+        ' 
+        btn6.BackColor = SystemColors.InactiveCaptionText
+        btn6.BackgroundImageLayout = ImageLayout.Stretch
+        btn6.FlatStyle = FlatStyle.Popup
+        btn6.ForeColor = SystemColors.ButtonHighlight
+        btn6.Location = New Point(392, 282)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(44, 38)
+        btn6.TabIndex = 44
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = False
+        ' 
+        ' btnEqual
+        ' 
+        btnEqual.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        btnEqual.BackgroundImageLayout = ImageLayout.Stretch
+        btnEqual.FlatStyle = FlatStyle.Popup
+        btnEqual.Location = New Point(442, 379)
+        btnEqual.Name = "btnEqual"
+        btnEqual.Size = New Size(44, 38)
+        btnEqual.TabIndex = 45
+        btnEqual.Text = "="
+        btnEqual.UseVisualStyleBackColor = False
+        ' 
+        ' btn3
+        ' 
+        btn3.BackColor = SystemColors.InactiveCaptionText
+        btn3.BackgroundImageLayout = ImageLayout.Stretch
+        btn3.FlatStyle = FlatStyle.Popup
+        btn3.ForeColor = SystemColors.ButtonHighlight
+        btn3.Location = New Point(392, 335)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(44, 38)
+        btn3.TabIndex = 46
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = False
+        ' 
+        ' btn2
+        ' 
+        btn2.BackColor = SystemColors.InactiveCaptionText
+        btn2.BackgroundImageLayout = ImageLayout.Stretch
+        btn2.FlatStyle = FlatStyle.Popup
+        btn2.ForeColor = SystemColors.ButtonHighlight
+        btn2.Location = New Point(342, 335)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(44, 38)
+        btn2.TabIndex = 47
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = False
+        ' 
+        ' btn0
+        ' 
+        btn0.BackColor = SystemColors.InactiveCaptionText
+        btn0.BackgroundImageLayout = ImageLayout.Stretch
+        btn0.FlatStyle = FlatStyle.Popup
+        btn0.ForeColor = SystemColors.ButtonHighlight
+        btn0.Location = New Point(342, 379)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(44, 38)
+        btn0.TabIndex = 48
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = False
+        ' 
+        ' btn00
+        ' 
+        btn00.BackColor = SystemColors.InactiveCaptionText
+        btn00.BackgroundImageLayout = ImageLayout.Stretch
+        btn00.FlatStyle = FlatStyle.Popup
+        btn00.ForeColor = SystemColors.ButtonHighlight
+        btn00.Location = New Point(292, 379)
+        btn00.Name = "btn00"
+        btn00.Size = New Size(44, 38)
+        btn00.TabIndex = 49
+        btn00.Text = "00"
+        btn00.UseVisualStyleBackColor = False
+        ' 
+        ' btn5
+        ' 
+        btn5.BackColor = SystemColors.InactiveCaptionText
+        btn5.BackgroundImageLayout = ImageLayout.Stretch
+        btn5.FlatStyle = FlatStyle.Popup
+        btn5.ForeColor = SystemColors.ButtonHighlight
+        btn5.Location = New Point(342, 282)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(44, 38)
+        btn5.TabIndex = 50
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = False
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        btnAdd.BackgroundImageLayout = ImageLayout.Stretch
+        btnAdd.FlatStyle = FlatStyle.Popup
+        btnAdd.Location = New Point(442, 335)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(44, 38)
+        btnAdd.TabIndex = 51
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = False
+        ' 
+        ' btnDot
+        ' 
+        btnDot.BackColor = SystemColors.InactiveCaptionText
+        btnDot.BackgroundImageLayout = ImageLayout.Stretch
+        btnDot.FlatStyle = FlatStyle.Popup
+        btnDot.ForeColor = SystemColors.ButtonHighlight
+        btnDot.Location = New Point(392, 379)
+        btnDot.Name = "btnDot"
+        btnDot.Size = New Size(44, 38)
+        btnDot.TabIndex = 52
+        btnDot.Text = "."
+        btnDot.UseVisualStyleBackColor = False
+        ' 
+        ' btnSub
+        ' 
+        btnSub.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        btnSub.BackgroundImageLayout = ImageLayout.Stretch
+        btnSub.FlatStyle = FlatStyle.Popup
+        btnSub.Location = New Point(442, 282)
+        btnSub.Name = "btnSub"
+        btnSub.Size = New Size(44, 38)
+        btnSub.TabIndex = 56
+        btnSub.Text = "-"
+        btnSub.UseVisualStyleBackColor = False
+        ' 
+        ' btn1
+        ' 
+        btn1.BackColor = SystemColors.InactiveCaptionText
+        btn1.BackgroundImageLayout = ImageLayout.Stretch
+        btn1.FlatStyle = FlatStyle.Popup
+        btn1.ForeColor = SystemColors.ButtonHighlight
+        btn1.Location = New Point(292, 335)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(44, 38)
+        btn1.TabIndex = 57
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = False
+        ' 
+        ' btn4
+        ' 
+        btn4.BackColor = SystemColors.InactiveCaptionText
+        btn4.BackgroundImageLayout = ImageLayout.Stretch
+        btn4.FlatStyle = FlatStyle.Popup
+        btn4.ForeColor = SystemColors.ButtonHighlight
+        btn4.Location = New Point(292, 282)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(44, 38)
+        btn4.TabIndex = 60
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = False
+        ' 
+        ' btnLog
+        ' 
+        btnLog.BackColor = SystemColors.InactiveCaptionText
+        btnLog.BackgroundImageLayout = ImageLayout.Stretch
+        btnLog.FlatStyle = FlatStyle.Popup
+        btnLog.ForeColor = SystemColors.ButtonHighlight
+        btnLog.Location = New Point(242, 238)
+        btnLog.Name = "btnLog"
+        btnLog.Size = New Size(44, 38)
+        btnLog.TabIndex = 77
+        btnLog.Text = "Log"
+        btnLog.UseVisualStyleBackColor = False
+        ' 
+        ' btnCos
+        ' 
+        btnCos.BackColor = Color.Silver
+        btnCos.BackgroundImageLayout = ImageLayout.Stretch
+        btnCos.FlatStyle = FlatStyle.Popup
+        btnCos.Location = New Point(342, 150)
+        btnCos.Name = "btnCos"
+        btnCos.Size = New Size(44, 38)
+        btnCos.TabIndex = 78
+        btnCos.Text = "Cos"
+        btnCos.UseVisualStyleBackColor = False
+        ' 
+        ' btnTan
+        ' 
+        btnTan.BackColor = Color.Silver
+        btnTan.BackgroundImageLayout = ImageLayout.Stretch
+        btnTan.FlatStyle = FlatStyle.Popup
+        btnTan.Location = New Point(392, 150)
+        btnTan.Name = "btnTan"
+        btnTan.Size = New Size(44, 38)
+        btnTan.TabIndex = 79
+        btnTan.Text = "Tan"
+        btnTan.UseVisualStyleBackColor = False
+        ' 
+        ' btnRad
+        ' 
+        btnRad.BackColor = Color.Silver
+        btnRad.BackgroundImageLayout = ImageLayout.Stretch
+        btnRad.FlatStyle = FlatStyle.Popup
+        btnRad.Location = New Point(442, 150)
+        btnRad.Name = "btnRad"
+        btnRad.Size = New Size(44, 38)
+        btnRad.TabIndex = 80
+        btnRad.Text = "Rad"
+        btnRad.UseVisualStyleBackColor = False
+        ' 
+        ' btnSin
+        ' 
+        btnSin.BackColor = Color.Silver
+        btnSin.BackgroundImageLayout = ImageLayout.Stretch
+        btnSin.FlatStyle = FlatStyle.Popup
+        btnSin.Location = New Point(292, 150)
+        btnSin.Name = "btnSin"
+        btnSin.Size = New Size(44, 38)
+        btnSin.TabIndex = 81
+        btnSin.Text = "Sin"
+        btnSin.UseVisualStyleBackColor = False
+        ' 
+        ' btnLn
+        ' 
+        btnLn.BackColor = Color.Silver
+        btnLn.BackgroundImageLayout = ImageLayout.Stretch
+        btnLn.FlatStyle = FlatStyle.Popup
+        btnLn.Location = New Point(242, 194)
+        btnLn.Name = "btnLn"
+        btnLn.Size = New Size(44, 38)
+        btnLn.TabIndex = 82
+        btnLn.Text = "ln"
+        btnLn.UseVisualStyleBackColor = False
+        ' 
+        ' btnInv
+        ' 
+        btnInv.BackColor = Color.Silver
+        btnInv.BackgroundImageLayout = ImageLayout.Stretch
+        btnInv.FlatStyle = FlatStyle.Popup
+        btnInv.Location = New Point(242, 150)
+        btnInv.Name = "btnInv"
+        btnInv.Size = New Size(44, 38)
+        btnInv.TabIndex = 83
+        btnInv.Text = "Inv"
+        btnInv.UseVisualStyleBackColor = False
+        ' 
+        ' btnDeg
+        ' 
+        btnDeg.BackColor = SystemColors.InactiveCaptionText
+        btnDeg.BackgroundImageLayout = ImageLayout.Stretch
+        btnDeg.FlatStyle = FlatStyle.Popup
+        btnDeg.ForeColor = SystemColors.ButtonHighlight
+        btnDeg.Location = New Point(242, 282)
+        btnDeg.Name = "btnDeg"
+        btnDeg.Size = New Size(44, 38)
+        btnDeg.TabIndex = 84
+        btnDeg.Text = "Deg"
+        btnDeg.UseVisualStyleBackColor = False
+        ' 
+        ' btnpi
+        ' 
+        btnpi.BackColor = SystemColors.InactiveCaptionText
+        btnpi.BackgroundImageLayout = ImageLayout.Stretch
+        btnpi.FlatStyle = FlatStyle.Popup
+        btnpi.ForeColor = SystemColors.ButtonHighlight
+        btnpi.Location = New Point(242, 335)
+        btnpi.Name = "btnpi"
+        btnpi.Size = New Size(44, 38)
+        btnpi.TabIndex = 85
+        btnpi.Text = "π"
+        btnpi.UseVisualStyleBackColor = False
+        ' 
+        ' btnsqrt
+        ' 
+        btnsqrt.BackColor = SystemColors.InactiveCaptionText
+        btnsqrt.BackgroundImageLayout = ImageLayout.Stretch
+        btnsqrt.FlatStyle = FlatStyle.Popup
+        btnsqrt.ForeColor = SystemColors.ButtonHighlight
+        btnsqrt.Location = New Point(242, 379)
+        btnsqrt.Name = "btnsqrt"
+        btnsqrt.Size = New Size(44, 38)
+        btnsqrt.TabIndex = 86
+        btnsqrt.Text = "√"
+        btnsqrt.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(242, 90)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(244, 44)
+        TextBox1.TabIndex = 87
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(686, 499)
+        Controls.Add(TextBox1)
+        Controls.Add(btnsqrt)
+        Controls.Add(btnpi)
+        Controls.Add(btnDeg)
+        Controls.Add(btnInv)
+        Controls.Add(btnLn)
+        Controls.Add(btnSin)
+        Controls.Add(btnRad)
+        Controls.Add(btnTan)
+        Controls.Add(btnCos)
+        Controls.Add(btnLog)
+        Controls.Add(btn4)
+        Controls.Add(btn1)
+        Controls.Add(btnSub)
+        Controls.Add(btnDot)
+        Controls.Add(btnAdd)
+        Controls.Add(btn5)
+        Controls.Add(btn00)
+        Controls.Add(btn0)
+        Controls.Add(btn2)
+        Controls.Add(btn3)
+        Controls.Add(btnEqual)
+        Controls.Add(btn6)
+        Controls.Add(btnBack)
+        Controls.Add(btnAC)
+        Controls.Add(btnD)
+        Controls.Add(btn7)
+        Controls.Add(btnMul)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btnPer)
+        Font = New Font("Times New Roman", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button33_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Friend WithEvents btnPer As RoundButton
+    Friend WithEvents btn8 As RoundButton
+    Friend WithEvents btn9 As RoundButton
+    Friend WithEvents btnMul As RoundButton
+    Friend WithEvents btn7 As RoundButton
+    Friend WithEvents btnD As RoundButton
+    Friend WithEvents btnAC As RoundButton
+    Friend WithEvents btnBack As RoundButton
+    Friend WithEvents btn6 As RoundButton
+    Friend WithEvents btnEqual As RoundButton
+    Friend WithEvents btn3 As RoundButton
+    Friend WithEvents btn2 As RoundButton
+    Friend WithEvents btn0 As RoundButton
+    Friend WithEvents btn00 As RoundButton
+    Friend WithEvents btn5 As RoundButton
+    Friend WithEvents btnAdd As RoundButton
+    Friend WithEvents btnDot As RoundButton
+    Friend WithEvents btnSub As RoundButton
+    Friend WithEvents btn1 As RoundButton
+    Friend WithEvents btn4 As RoundButton
+
+    Private Sub RoundButton16_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub RoundButton1_Click(sender As Object, e As EventArgs) Handles btnPer.Click
+
+    End Sub
+
+    Friend WithEvents btnLog As RoundButton
+    Friend WithEvents btnCos As RoundButton
+    Friend WithEvents btnTan As RoundButton
+    Friend WithEvents btnRad As RoundButton
+    Friend WithEvents btnSin As RoundButton
+    Friend WithEvents btnLn As RoundButton
+    Friend WithEvents btnInv As RoundButton
+    Friend WithEvents btnDeg As RoundButton
+    Friend WithEvents btnpi As RoundButton
+    Friend WithEvents btnsqrt As RoundButton
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ EXP-5/forms.cs/Form1.vb
@@ -0,0 +1,159 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ButtonClick(sender As Object, e As EventArgs) Handles btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click, btn0.Click, btn00.Click, btnDot.Click, btnAdd.Click, btnSub.Click, btnMul.Click, btnD.Click, btnSin.Click, btnCos.Click, btnTan.Click, btnLog.Click, btnLn.Click, btnInv.Click, btnDeg.Click, btnPer.Click, btnpi.Click, btnsqrt.Click, btnEqual.Click, btnRad.Click, btnBack.Click, btn6.Click, btnAC.Click, btnBack.Click
+        Dim button = CType(sender, Button)
+        If button.Name = "btn1" Then
+            TextBox1.Text = TextBox1.Text + "1"
+        End If
+        If button.Name = "btn2" Then
+            TextBox1.Text = TextBox1.Text + "2"
+        End If
+        If button.Name <> "btn3" Then
+        Else
+            TextBox1.Text = TextBox1.Text + "3"
+        End If
+        If button.Name = "btn4" Then
+            TextBox1.Text = TextBox1.Text + "4"
+        End If
+        If button.Name = "btn5" Then
+            TextBox1.Text = TextBox1.Text + "5"
+        End If
+        If button.Name = "btn6" Then
+            TextBox1.Text = TextBox1.Text + "6"
+        End If
+        If button.Name = "btn7" Then
+            TextBox1.Text = TextBox1.Text + "7"
+        End If
+        If button.Name = "btn8" Then
+            TextBox1.Text = TextBox1.Text + "8"
+        End If
+        If button.Name = "btn9" Then
+            TextBox1.Text = TextBox1.Text + "9"
+        End If
+        If button.Name = "btn0" Then
+            TextBox1.Text = TextBox1.Text + "0"
+        End If
+        If button.Name = "btn00" Then
+            TextBox1.Text = TextBox1.Text + "00"
+        End If
+        If button.Name = "btnAdd" Then
+            TextBox1.Text = TextBox1.Text + "+"
+        End If
+        If button.Name = "btnDot" Then
+            TextBox1.Text = TextBox1.Text + "."
+        End If
+        If button.Name = "btnSub" Then
+            TextBox1.Text = TextBox1.Text + "-"
+        End If
+        If button.Name = "btnMul" Then
+            TextBox1.Text = TextBox1.Text + "*"
+        End If
+        If button.Name = "btnD" Then
+            TextBox1.Text = TextBox1.Text + "/"
+        End If
+        If button.Name = "btnAC" Then
+            TextBox1.Text = ""
+        End If
+        If button.Name = "btnEqual" Then
+            Dim str As String = TextBox1.Text
+            Dim result = New DataTable().Compute(str, Nothing)
+            TextBox1.Text = result
+        End If
+        If button.Name = "btnSin" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = Math.Sin(value * Math.PI / 180).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnCos" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = Math.Cos(value * Math.PI / 180).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnTan" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = Math.Tan(value * Math.PI / 180).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnLog" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = Math.Log10(value).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnln" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = Math.Log(value).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnSqrt" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = Math.Sqrt(value).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnRad" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = (value * Math.PI / 180).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnDeg" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = (value * 180 / Math.PI).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnpi" Then
+            Try
+                TextBox1.Text = Math.PI.ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnPer" Then
+            Try
+                Dim value = CDbl(TextBox1.Text)
+                TextBox1.Text = (value / 100).ToString
+            Catch ex As Exception
+                TextBox1.Text = "Error"
+            End Try
+        End If
+        If button.Name = "btnBack" Then
+            If TextBox1.Text.Length > 0 Then
+                TextBox1.Text = TextBox1.Text.Substring(0, TextBox1.Text.Length - 1)
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnDiv_Click(sender As Object, e As EventArgs) Handles btnD.Click
+
+    End Sub
+
+    Private Sub RoundButton1_Click_1(sender As Object, e As EventArgs) Handles btnpi.Click
+
+    End Sub
+End Class
