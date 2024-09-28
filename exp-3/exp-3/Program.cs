f2877744b578b415793fa4adf79da50237d92245
suryateja1296 <166375949+suryateja1296@users.noreply.github.com>
2024-09-28 14:37:33 +0530

    Add files via upload

---
+++ exp-3/exp-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+class TEJ
+{
+    static void Main()
+    {
+        Thread t = new Thread(Worker);
+
+        t.Start();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Marvel is better");
+            Thread.Sleep(100);
+        }
+
+        t.Join();
+
+        Console.WriteLine("Done");
+    }
+
+    static void Worker()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("DC is good");
+            Thread.Sleep(100);
+        }
+    }
+}
